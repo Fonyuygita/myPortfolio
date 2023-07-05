b44c1c86dc16a0e63981d9e297e2c4edcc28f81f
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-07-04 21:33:49 -0700

    first commit today

--- src/component/productLlist/ProductList.jsx
+++ src/component/productLlist/ProductList.jsx
@@ -6,9 +6,9 @@ const ProductList = () => {
   return (
     <div className="pl">
       <div className="pl-texts">
-        <h1 className="pl-title">{`Create & inspire. It's Lama`}</h1>
+        <h1 className="pl-title">{`My Projects. Create & inspire. It's Gita`}</h1>
         <p className="pl-desc">
-          Lama is a creative portfolio that your work has been waiting for.
+          Gita is a creative portfolio that your work has been waiting for.
           Beautiful homes, stunning portfolio styles & a whole lot more awaits
           inside.
         </p>
@@ -17,6 +17,7 @@ const ProductList = () => {
         {products.map((item) => (
           <Product key={item.id} img={item.img} link={item.link} />
         ))}
+        <a href="#">See more on Github</a>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -14,19 +14,19 @@ export const products = [
       img: "",
       link: "http://lama.dev",
     },
-    {
-      id: 4,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 5,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 6,
-      img: "",
-      link: "http://lama.dev",
-    },
+    // {
+    //   id: 4,
+    //   img: "",
+    //   link: "http://lama.dev",
+    // },
+    // {
+    //   id: 5,
+    //   img: "",
+    //   link: "http://lama.dev",
+    // },
+    // {
+    //   id: 6,
+    //   img: "",
+    //   link: "http://lama.dev",
+    // },
   ];
